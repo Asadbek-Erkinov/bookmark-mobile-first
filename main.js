1b3ced134ee7749389f990f0c493158333716df9
Asadbek-Erkinov <asadbekeekinovg777@gmail.com>
2024-09-30 05:10:00 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var elSiteHeader = document.querySelector("js-header");
+
+var elShowMenuBtn = document.querySelector("hambur-menu-btn");
+
+
+
+elShowMenuBtn.addEventListener("click", function() {
+
+
+
+})
