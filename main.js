d4a77bd3b7d7cacbcafcc9b2b654625c84c22375
Asadbek-Erkinov <asadbekeekinovg777@gmail.com>
2024-10-04 12:21:28 +0500

    another commit

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var elSiteHeader = document.querySelector("js-header");
+var elSiteHeader = document.querySelector(".js-header");
 
-var elShowMenuBtn = document.querySelector("hambur-menu-btn");
+var elShowMenuBtn = elSiteHeader.querySelector(".hambur-menu-btn");
 
 
 
 elShowMenuBtn.addEventListener("click", function() {
 
+elSiteHeader.classList.toggle("show-menu");
 
+document.body.classList.toggle("no-scroll")
 
 })
